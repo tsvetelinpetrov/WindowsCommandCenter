b8f1e53b921cee1659d4fd45796b6cd4e7953c62
Tsvetelin <cvetelin626@gmail.com>
2022-01-29 23:14:09 +0200

    Implemented brightness adjust by slider

--- WindowsCommandCenter/MainWindow.xaml.cs
+++ WindowsCommandCenter/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WindowsCommandCenter
 {
@@ -20,9 +9,22 @@ namespace WindowsCommandCenter
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        MonitorsController monitorsController = new MonitorsController();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            brightnessLabel.Content = monitorsController.Get().ToString() + "%";
+            brightnessSlider.Value = monitorsController.Get();
+        }
+
+        private void brightnessSlider_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            UInt32 newVal = Convert.ToUInt32(brightnessSlider.Value);
+            monitorsController.Set(newVal);
+            brightnessLabel.Content = newVal.ToString() + "%";
         }
     }
 }
